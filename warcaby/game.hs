f3f2fbfc8f2350b03c5c49170f8ba6a849081d51
Karol Bisztyga <karolbisztyga@gmail.com>
2016-05-21 15:16:26 +0100

    asd

--- warcaby/game.hs
+++ warcaby/game.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 signs = take 8 ['a'..]
 numbers = take 8 [1..]
 b = createBoard
@@ -8,6 +10,7 @@ indexOf' el list curr
 	| el == list!!curr = curr
 	| otherwise = indexOf' el list (curr+1)
 
+createBoard :: [[Int]]
 createBoard = createBoard' 0 []
 createBoard' n list
 	| n >= length numbers = []
@@ -17,9 +20,25 @@ getFieldValue sign number board
 	| number > 8 || number < 0 || sign > 'h' || sign < 'a' = -1
 	| otherwise = board!!(indexOf sign signs)!!number
 
---rozmieszcza pionki na planszy
-prepareGame board = prepareGame' board 1
-prepareGame' board player currentPaw
-	| 
-	| 
-	| 
+setFieldValue sign number board newValue = 
+		setFieldValueSgn sign number board newValue (ord 'a')
+
+setFieldValueSgn sgn num (b:brd) val currSgn
+	| ord sgn == currSgn = (setFieldValueNum num b val 0):brd
+	| otherwise = b:(setFieldValueSgn sgn num brd val (currSgn+1))
+
+setFieldValueNum num (b:brd) val currNum
+	| currNum == num = val:brd
+	| otherwise = b:setFieldValueNum num brd val (currNum+1)
+
+prepareGame board = prepareGame' board 1 0
+prepareGame' board currPlayer currPaw
+	| currPlayer <= 2 && currPaw < 8 =
+		prepareGame' (setFieldValue (chr (97+currPaw)) 
+		((currPlayer-1)*6 + mod currPaw 2) board currPlayer) 
+		currPlayer (currPaw+1)
+	| currPlayer <= 2 && currPaw >= 8 = 
+		prepareGame' board (currPlayer+1) 0
+	| currPlayer > 2 = board
+
+--makeMove fromSign fromNumber toSign toNumber board = 
