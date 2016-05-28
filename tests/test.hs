9837a93a8a8fbdc8c3f3e696ca4a251595871282
Karol Bisztyga <karolbisztyga@gmail.com>
2016-05-28 22:56:35 +0100

    Asd

--- tests/test.hs
+++ tests/test.hs
@@ -2,3 +2,19 @@ m = ml 0 []
 ml c l
 	| c<10 = ml (c+1) (c:l)
 	| otherwise = l
+
+z n
+	| n<10 = Just (n*2) 
+	| otherwise = Nothing
+
+a = do
+	x <- getLine
+	putStrLn(x++" hehe")
+
+rev str = rev' str []
+rev' [] res = []
+rev' str res = (last str):rev' (take (length str-1) str) res
+
+data Tree a = E | L a | N (Tree a) a (Tree a)
+	deriving (Eq, Show, Ord)
+
--- warcaby/game.hs
+++ warcaby/game.hs
@@ -59,4 +59,30 @@ makeMove player fromSign fromNumber toSign toNumber board
 	where
 		valFrom = getFieldValue fromSign fromNumber board
 		valTo = getFieldValue toSign toNumber board
+{-
+--zwraca liste krotek(2) z mozliwosciami ruchu i wartosciami gry
+--po kazdym z nich
+getPossibleMoves currSgn currNum board moves
+	|  = 
+	|  = 
+	where 
+-}
 
+getPawsPositions player board = getPawsPositions' player board 'a' 0 []
+getPawsPositions' pl brd cs cn list
+	| val == -1 && cs > 'h' = []
+	| val == -1 && cn > 7 = getPawsPositions' pl brd (chr ((ord cs)+1)) 0 list
+	| val /= pl = getPawsPositions' pl brd cs (cn+1) list
+	| val == pl = [(cs,cn)]:getPawsPositions' pl brd cs (cn+1) list
+	where val = getFieldValue cs cn brd
+
+getPawMoves player board pawSign pawNum
+	| getFieldValue pawSign pawNum board /= player = error "no such paw here"
+	| otherwise = getPawMoves' player board pawSign pawNum [] 0
+getPawMoves' pl brd ps pn list jumpCount = 
+	| pl == 1 && getFieldValue (chr ((ord ps)+1)) (pn+1) brd == -1 = []
+	| pl == 1 && getFieldValue (chr ((ord ps)+1)) (pn-1) brd == -1 = []
+	| pl == 1 && getFieldValue (chr ((ord ps)+1)) (pn-1) brd == -1 = []
+	| pl == 1 &&  = 
+	| pl == 2 &&  = 
+	| pl == 2 &&  = 
