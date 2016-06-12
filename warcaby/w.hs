caf4a16125fcab681484e31560728f9747b8a96d
Karol Bisztyga <karolbisztyga@gmail.com>
2016-06-12 23:52:10 +0200

    asd

--- warcaby/w.hs
+++ warcaby/w.hs
@@ -71,11 +71,6 @@ printGame' b w = printGame'' (getGameString b w) "" '1'
 printGame'' "" res n = "_abcdefgh_\n"++res++"_abcdefgh_\n"
 printGame'' str res n = printGame'' (drop 8 str) (res++[n]++(take 8 str)++[n]++"\n") (chr ((ord n)+1))
 
-test 0 = []
-test n
-	| n == 4 = (chr(ord('a')+n),n):(test (n-1))
-	| otherwise = (chr(ord('a')+n),n):(test (n-1))
-
 canPawBeat paws opponentPaws dir currSgn currNum
 	| not (canPawMove (paws++opponentPaws) dir currSgn currNum 2) = False
 	| dir==UP && (chr ((ord currSgn)+2))<='h' && (currNum-2)>=1 &&
@@ -188,14 +183,26 @@ checkBeat dir paws opponentPaws from (to:toArr)
 
 --wyszukuje zbior ruchow rownowaznych o najwyzszym priorytecie czyli najpierw szuka bic
 --a jak nie ma bic to szuka zywklych ruchow
-{-}
-aiMakeMove paws opponentPaws dir moveNumber
-	| fst beatchosen /= '0' = aiBeat paws opponentPaws (fst beatchosen) (snd beatchosen)
-	| otherwise = aiMove paws opponentPaws '0' 0
+--zwraca krotke: (paws,opponentpaws)
+aiMakeMove paws opponentPaws dir movesCounter
+	| fst beatchosen /= '0' = aiBeat paws opponentPaws (snd (getPawBeats paws opponentPaws dir (fst beatchosen) (snd beatchosen))) ((fst beatchosen),(snd beatchosen))
+	| otherwise = (movePaw paws opponentPaws (fst moveChosen) (snd moveChosen) (fst gpm) (snd gpm),opponentPaws)
 	where
-		m = getPawsMoves paws opponentPaws dir
-		beatchosen = aiChooseBeat paws opponentPaws dir
--}
+		moveChosen = aiChooseMove paws opponentPaws dir movesCounter
+		beatchosen = aiChooseBeat paws opponentPaws dir movesCounter
+		gpm = getPawMove paws opponentPaws (fst moveChosen) (snd moveChosen) dir
+
+aiBeat paws opponentPaws [] current = (paws,opponentPaws)
+aiBeat paws opponentPaws (p:path) current = 
+	aiBeat (movePaw paws opponentPaws currSgn currNum destSgn destNum)
+	(removePaw opponentPaws (chr (floor((fromIntegral (ord(destSgn)+ord(currSgn)))/2))) (floor((fromIntegral (currNum+destNum))/2)))
+	path p
+	where
+		currSgn = fst current
+		currNum = snd current
+		destSgn = fst p
+		destNum = snd p
+
 getRandomElement list n = list!!(rnd n 0 (length list - 1))
 --x = RandomRIO (1,10)
 
@@ -205,18 +212,18 @@ getGameValue paws opponentPaws dir =
 
 
 --wybiera najlepsze bicie
-aiChooseBeat paws opponentPaws dir
+aiChooseBeat paws opponentPaws dir movesCounter
 	| length pb == 0 = ('0',0)
-	| otherwise = aiChooseBeat' paws opponentPaws dir (init pb) (last pb)
+	| otherwise = getRandomElement (aiChooseBeat' paws opponentPaws dir pb 0 []) movesCounter
 	where pb = getPawsBeats paws opponentPaws dir
-aiChooseBeat' paws opponentPaws dir [] currentMax = currentMax
-aiChooseBeat' paws opponentPaws dir (b:beats) currentMax
-	| getPawBeats paws opponentPaws dir sgnCM numCM < getPawBeats paws opponentPaws dir sgnB numB = 
-		aiChooseBeat' paws opponentPaws dir beats b
-	| otherwise = aiChooseBeat' paws opponentPaws dir beats currentMax
+aiChooseBeat' paws opponentPaws dir [] currentMax list = list
+aiChooseBeat' paws opponentPaws dir (b:beats) currentMax list
+	| currentMax < fst (getPawBeats paws opponentPaws dir sgnB numB) = 
+		aiChooseBeat' paws opponentPaws dir beats (fst (getPawBeats paws opponentPaws dir sgnB numB)) [b]
+	| currentMax == fst (getPawBeats paws opponentPaws dir sgnB numB) = 
+		aiChooseBeat' paws opponentPaws dir beats currentMax (b:list)
+	| otherwise = aiChooseBeat' paws opponentPaws dir beats currentMax list
 	where
-		sgnCM = fst currentMax
-		numCM = snd currentMax
 		sgnB = fst b
 		numB = snd b
 
@@ -263,11 +270,15 @@ aiChooseMove' paws opponentPaws dir (m:moves) currVal list
 		currNum = snd m
 		destSgn = fst (getPawMove paws opponentPaws (fst m) (snd m) dir)
 		destNum = snd (getPawMove paws opponentPaws (fst m) (snd m) dir)
-
-aiBeat paws opponentPaws sgn num = "beating"
-aiMove paws opponentPaws sgn num = "moving"
-
-
+{-}
+aiMove paws opponentPaws dir movesCounter 
+	| length beats /= 0 = aiChooseBeat paws opponentPaws dir movesCounter
+	| length moves /= 0 = aiChooseMove paws opponentPaws dir movesCounter
+	| otherwise = error "no possible moves!"
+	where
+		beats = getPawsBeats paws opponentPaws dir
+		moves = getPawsMoves paws opponentPaws dir
+-}
 {-}
 ww = movePaw (movePaw w b 'f' 3 'f' 5) b 'c' 2 'f' 3
 bb = movePaw (movePaw b w 'e' 6 'e' 4) w 'b' 7 'e' 6
@@ -280,9 +291,47 @@ die str f = do
 
 colors = ["white","black"]
 
+data MODE = AIVSAI | AIVSMAN | MANVSAI | MANVSMAN deriving(Show,Ord,Eq)
+
+
+
 ---------------------------------------------------------------------
 
-orderMove moveCounter blacks whites str = do
+nextHop moveCounter blacks whites str mode
+	| mode == AIVSAI = getMoveFromAi moveCounter blacks whites str mode
+	| mode == MANVSMAN = orderMove moveCounter blacks whites str mode
+	| mode == AIVSMAN && mod moveCounter 2 == 0 = getMoveFromAi moveCounter blacks whites str mode
+	| mode == AIVSMAN && mod moveCounter 2 == 1 = orderMove moveCounter blacks whites str mode
+	| mode == MANVSAI && mod moveCounter 2 == 0 = orderMove moveCounter blacks whites str mode
+	| mode == MANVSAI && mod moveCounter 2 == 1 = getMoveFromAi moveCounter blacks whites str mode
+
+getMoveFromAi moveCounter blacks whites str mode = do
+	system "clear"
+	printGame blacks whites
+	putStrLn("move "++(show moveCounter)++" press enter to get ai move")
+	c <- getLine
+	if(moveCounter `mod` 2 == 0)
+		then nextHop (moveCounter+1) (snd wm) (fst wm) str mode
+		else nextHop (moveCounter+1) (fst bm) (snd bm) str mode
+		where
+			wm = (aiMakeMove whites blacks DOWN moveCounter)
+			bm = (aiMakeMove blacks whites UP moveCounter)
+		{-}
+		then if length getPawsBeats whites blacks DOWN > 0
+			then nextHop (moveCounter+1) blacks (whites) str mode--beat
+			else if length getPawsMoves whites blacks DOWN > 0
+				then nextHop (moveCounter+1) blacks (whites) str mode--move
+				else gameOver --game over
+		else if length getPawsBeats blacks whites UP == 0
+			then  nextHop (moveCounter+1) (blacks) whites str mode--beat
+			else if length getPawsMoves blacks whites UP > 0
+				then nextHop (moveCounter+1) (blacks) whites str mode--move
+				else gameOver --game over
+-}
+over = do
+	putStrLn("gameOver")
+
+orderMove moveCounter blacks whites str mode = do
 	system "clear"
 	printGame blacks whites
 	putStrLn("move ["++show(moveCounter+1)++"] "++colors!!turn++"s to move")
@@ -293,51 +342,45 @@ orderMove moveCounter blacks whites str = do
 	if (length (splitOn "-" move) /= 2)
 		then --orderMove moveCounter blacks whites "wrong input"
 			if (length (splitOn "x" move) <= 1)
-				then orderMove moveCounter blacks whites "wrong input"
+				then orderMove moveCounter blacks whites "wrong input" mode
 				else
 					if turn == 0
-						then whitesBeat moveCounter blacks whites (splitOn "x" move)
-						else blacksBeat moveCounter blacks whites (splitOn "x" move)
+						then whitesBeat moveCounter blacks whites (splitOn "x" move) mode
+						else blacksBeat moveCounter blacks whites (splitOn "x" move) mode
 		else 
 			if turn == 0
-				then whitesMove moveCounter blacks whites ((splitOn "-" move)!!0) ((splitOn "-" move)!!1) --makeMove moveCounter 0 blacks whites ((splitOn "-" move)!!0) ((splitOn "-" move)!!1)
-				else blacksMove moveCounter blacks whites ((splitOn "-" move)!!0) ((splitOn "-" move)!!1)
+				then whitesMove moveCounter blacks whites ((splitOn "-" move)!!0) ((splitOn "-" move)!!1) mode --makeMove moveCounter 0 blacks whites ((splitOn "-" move)!!0) ((splitOn "-" move)!!1)
+				else blacksMove moveCounter blacks whites ((splitOn "-" move)!!0) ((splitOn "-" move)!!1) mode
 	where 
 		turn = (moveCounter `mod` 2)
-{-}
-getMoveFromAi moveCounter blacks whites str = do
-	system "clear"
-	printGame blacks whites
-	putStrLn("press a key to get ai move")
-	c <- getChar
--}
+
 ---------------------------------------------------------------------
 
-blacksMove moveCounter blacks whites from to = do
+blacksMove moveCounter blacks whites from to mode = do
 	if not (isMoveAvailable (blacks++whites) UP sgnFrom numFrom sgnTo numTo) || (movePaw blacks whites sgnFrom numFrom sgnTo numTo) == []
-		then orderMove moveCounter blacks whites "*could not make that move"
-		else orderMove (moveCounter+1) (movePaw blacks whites sgnFrom numFrom sgnTo numTo) whites
-				("blacks moved from ("++[sgnFrom]++","++(show numFrom)++") to ("++[sgnTo]++","++(show numTo)++")")
-	orderMove (moveCounter+1) blacks whites ""
+		then orderMove moveCounter blacks whites "*could not make that move" mode
+		else nextHop (moveCounter+1) (movePaw blacks whites sgnFrom numFrom sgnTo numTo) whites
+				("blacks moved from ("++[sgnFrom]++","++(show numFrom)++") to ("++[sgnTo]++","++(show numTo)++")") mode
+	nextHop (moveCounter+1) blacks whites "" mode
 	where
 		sgnFrom = fst (getFieldByCode (read from :: Float))
 		numFrom = snd (getFieldByCode (read from :: Float))
 		sgnTo = fst (getFieldByCode (read to :: Float))
 		numTo = snd (getFieldByCode (read to :: Float))
 
-blacksBeat moveCounter blacks whites (a:arr) = do
+blacksBeat moveCounter blacks whites (a:arr) mode = do
 	if not (checkBeat UP blacks whites a arr)
-		then orderMove moveCounter blacks whites "*could not make that beat"
-		else blacksBeat' moveCounter blacks whites a arr ""
+		then orderMove moveCounter blacks whites "*could not make that beat" mode
+		else blacksBeat' moveCounter blacks whites a arr "" mode
 
-blacksBeat' moveCounter blacks whites from [] str = do
-	orderMove (moveCounter+1) blacks whites str
-blacksBeat' moveCounter blacks whites from (to:toArr) str = do
+blacksBeat' moveCounter blacks whites from [] str mode = do
+	nextHop (moveCounter+1) blacks whites str mode
+blacksBeat' moveCounter blacks whites from (to:toArr) str mode = do
 	blacksBeat' moveCounter 
 		(movePaw blacks whites sgnFrom numFrom sgnTo numTo)
 		(removePaw whites (chr (floor((fromIntegral (ord(sgnTo)+ord(sgnFrom)))/2))) (floor((fromIntegral (numFrom+numTo))/2)))
 		to toArr
-		(str++"blacks beat from ("++[sgnFrom]++","++(show numFrom)++") to ("++[sgnTo]++","++(show numTo)++")\n")
+		(str++"blacks beat from ("++[sgnFrom]++","++(show numFrom)++") to ("++[sgnTo]++","++(show numTo)++")\n") mode
 	where
 		sgnFrom = fst (getFieldByCode (read from :: Float))
 		numFrom = snd (getFieldByCode (read from :: Float))
@@ -346,30 +389,30 @@ blacksBeat' moveCounter blacks whites from (to:toArr) str = do
 
 ---------------------------------------------------------------------
 
-whitesMove moveCounter blacks whites from to = do
+whitesMove moveCounter blacks whites from to mode = do
 	if not (isMoveAvailable (blacks++whites) DOWN sgnFrom numFrom sgnTo numTo) || ((movePaw whites blacks sgnFrom numFrom sgnTo numTo) == [])
-		then orderMove moveCounter blacks whites "*could not make that move"
-		else orderMove (moveCounter+1) blacks (movePaw whites blacks sgnFrom numFrom sgnTo numTo) 
-				("whites moved from ("++[sgnFrom]++","++(show numFrom)++") to ("++[sgnTo]++","++(show numTo)++")")
-	orderMove (moveCounter+1) blacks whites ""
+		then orderMove moveCounter blacks whites "*could not make that move" mode
+		else nextHop (moveCounter+1) blacks (movePaw whites blacks sgnFrom numFrom sgnTo numTo) 
+				("whites moved from ("++[sgnFrom]++","++(show numFrom)++") to ("++[sgnTo]++","++(show numTo)++")") mode
+	nextHop (moveCounter+1) blacks whites "" mode
 	where
 		sgnFrom = fst (getFieldByCode (read from :: Float))
 		numFrom = snd (getFieldByCode (read from :: Float))
 		sgnTo = fst (getFieldByCode (read to :: Float))
 		numTo = snd (getFieldByCode (read to :: Float))
 
-whitesBeat moveCounter blacks whites (a:arr) = do
+whitesBeat moveCounter blacks whites (a:arr) mode = do
 	if not (checkBeat DOWN whites blacks a arr)
-		then orderMove moveCounter blacks whites "*could not make that beat"
-		else whitesBeat' moveCounter blacks whites a arr ""
+		then orderMove moveCounter blacks whites "*could not make that beat" mode
+		else whitesBeat' moveCounter blacks whites a arr "" mode
 
-whitesBeat' moveCounter blacks whites from [] str = do
-	orderMove (moveCounter+1) blacks whites str
-whitesBeat' moveCounter blacks whites from (to:toArr) str = do
+whitesBeat' moveCounter blacks whites from [] str mode = do
+	nextHop (moveCounter+1) blacks whites str mode
+whitesBeat' moveCounter blacks whites from (to:toArr) str mode = do
 	whitesBeat' moveCounter 
 		(removePaw blacks (chr (floor((fromIntegral (ord(sgnTo)+ord(sgnFrom)))/2))) (floor((fromIntegral (numFrom+numTo))/2)))
 		(movePaw whites blacks sgnFrom numFrom sgnTo numTo) to toArr
-		(str++"whites beat from ("++[sgnFrom]++","++(show numFrom)++") to ("++[sgnTo]++","++(show numTo)++")\n")
+		(str++"whites beat from ("++[sgnFrom]++","++(show numFrom)++") to ("++[sgnTo]++","++(show numTo)++")\n") mode
 	where
 		sgnFrom = fst (getFieldByCode (read from :: Float))
 		numFrom = snd (getFieldByCode (read from :: Float))
@@ -378,29 +421,25 @@ whitesBeat' moveCounter blacks whites from (to:toArr) str = do
 
 ---------------------------------------------------------------------
 
-manVsMan = do
+begin mode = do
 	system "clear"
 	putStrLn("game started")
 	printGame blacks whites
-	orderMove 0 blacks whites ""
+	nextHop 0 blacks whites "" mode
 		where
-			whites = (('f',5):(zip (take 8 ['a'..]) $take 8 $cycle [2,1])++(zip(take 4 ['b','d','f','h']) $take 4 $cycle [3]))
+			whites = ((zip (take 8 ['a'..]) $take 8 $cycle [2,1])++(zip(take 4 ['b','d','f','h']) $take 4 $cycle [3]))
+				--(('f',5):(zip (take 8 ['a'..]) $take 8 $cycle [2,1])++(zip(take 4 ['b','d','f','h']) $take 4 $cycle [3]))
 				--((zip (take 8 ['a'..]) $take 8 $cycle [2,1])++(zip(take 4 ['b','d','f','h']) $take 4 $cycle [3]))
-			blacks = removePaw ((('c',4):(zip (take 8 ['a'..]) $take 8 $cycle [8,7])++(zip(take 4 ['a','c','e','g']) $take 4 $cycle [6]))) 'f' 7
+			blacks = ((zip (take 8 ['a'..]) $take 8 $cycle [8,7])++(zip(take 4 ['a','c','e','g']) $take 4 $cycle [6]))
+				--removePaw ((('c',4):(zip (take 8 ['a'..]) $take 8 $cycle [8,7])++(zip(take 4 ['a','c','e','g']) $take 4 $cycle [6]))) 'f' 7
 				--((zip (take 8 ['a'..]) $take 8 $cycle [8,7])++(zip(take 4 ['a','c','e','g']) $take 4 $cycle [6]))
 
-manVsAi = do 
-	putStrLn("...")
-
-
-aiVsMan = do 
-	putStrLn("...")
-
 gameStart = do
-	putStrLn("welcome!\nchoose game:\n[1] man vs man\n[2] man vs ai\n[3] ai vs man")
-	game <- getChar
-	if game == '1' then manVsMan else
-		if game == '2' then manVsAi else
-			if game == '3' then aiVsMan else
+	putStrLn("welcome!\nchoose game:\n[1] man vs man\n[2] man vs ai\n[3] ai vs man\n[4] ai vs ai")
+	game <- getLine
+	if game == "1" then begin MANVSMAN else
+		if game == "2" then begin MANVSAI else
+			if game == "3" then begin AIVSMAN else
+				if game == "4" then begin AIVSAI else
 				gameStart
 
